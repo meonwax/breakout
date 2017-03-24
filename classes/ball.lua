6e930ecb7fca4520b8de460f506138ed320dd3c2
meonwax <meonwax@meonwax.de>
2017-03-24 13:23:20 +0100

    Use normalized unit vector for ball direction

--- classes/ball.lua
+++ classes/ball.lua
@@ -1,20 +1,19 @@
 Ball = class {}
 
-Ball.speed = 200
+Ball.speed = 500
 Ball.radius = 5
 
 function Ball:init(position)
-  self.velocity = vector(-2, -1)
+  self.direction = vector(-2, -1):normalized() -- Set initial direction
   self.shape = HC.circle(position.x, position.y, Ball.radius)
 end
 
 function Ball:update(dt)
   local collisions = HC.collisions(self.shape)
   for otherShape, separatingVector in pairs(collisions) do
-    print(string.format("Collision. Separating vector = (%s,%s)", separatingVector.x, separatingVector.y))
-    self.velocity = vector(separatingVector.x, separatingVector.y)
+    self.direction = vector(separatingVector.x, separatingVector.y):normalized()
   end
-  self.shape:move((self.velocity * dt * Ball.speed):unpack())
+  self.shape:move((self.direction * dt * Ball.speed):unpack())
 end
 
 function Ball:draw()
