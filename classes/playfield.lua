ec8748fc63a86e72541c250b43da5c0bef7ecbb6
meonwax <meonwax@meonwax.de>
2017-03-25 23:15:08 +0100

    Code cleanup

--- classes/playfield.lua
+++ classes/playfield.lua
@@ -3,11 +3,11 @@ Playfield = class {}
 Playfield.borderWidth = 5
 
 function Playfield:init()
+  self.background = love.graphics.newImage("images/stars.jpg")
   self.borderTop = HC.rectangle(0, 0, love.graphics.getWidth(), Playfield.borderWidth)
   self.borderBottom = HC.rectangle(0, love.graphics.getHeight() - Playfield.borderWidth, love.graphics.getWidth(), Playfield.borderWidth)
   self.borderLeft = HC.rectangle(0, Playfield.borderWidth, Playfield.borderWidth, love.graphics.getHeight() - Playfield.borderWidth * 2)
   self.borderRight = HC.rectangle(love.graphics.getWidth() - Playfield.borderWidth, Playfield.borderWidth, Playfield.borderWidth, love.graphics.getHeight() - Playfield.borderWidth * 2)
-  self.background = love.graphics.newImage("images/stars.jpg")
 end
 
 function Playfield:update(dt)
@@ -15,6 +15,10 @@ end
 
 function Playfield:draw()
   self:drawBackground()
+  self:drawBorders()
+end
+
+function Playfield:drawBorders()
   love.graphics.setColor(colors.darkBlue)
   self.borderTop:draw('fill')
   self.borderBottom:draw('fill')
@@ -22,10 +26,8 @@ function Playfield:draw()
   self.borderRight:draw('fill')
 end
 
-
 function Playfield:drawBackground()
   local scaleFactor = love.graphics.getWidth() / self.background:getWidth()
-
   love.graphics.setColor(255, 255, 255, 255)
   love.graphics.draw(self.background, 0, 0, 0, scaleFactor, scaleFactor)
 end
